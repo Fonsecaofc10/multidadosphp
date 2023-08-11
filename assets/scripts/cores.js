d2dc4b7251de7f7f9b28297aaa6bf9a2de8e5d0e
Fonsecaofc10 <santosidiarte95@gmail.com>
2023-08-11 12:43:15 -0300

    commitmultidados

---
+++ assets/scripts/cores.js
@@ -0,0 +1,17 @@
+document.getElementById('btnVisualizarClientes').addEventListener('click', function() {
+    var tabelaSimples = document.getElementById('tabelaSimples');
+    tabelaSimples.className = "portlet box blue"; // Troque para a classe de cor desejada
+});
+
+document.getElementById('btnVisualizarUsuarios').addEventListener('click', function() {
+    var tabelaSimples = document.getElementById('tabelaSimples');
+    tabelaSimples.className = "portlet box green"; // Troque para a classe de cor desejada
+});
+
+document.getElementById('btnVisualizarFornecedores').addEventListener('click', function() {
+    var tabelaSimples = document.getElementById('tabelaSimples');
+    tabelaSimples.className = "portlet box purple"; // Troque para a classe de cor desejada
+});
+
+
+
